f31feaafa51ba68937a090c3aa9fa05a9d360051
chimitslucie <chimitslucie@gmail.com>
2025-02-11 15:49:04 +0100

    add nav component

---
+++ src/Components/nav.jsx
@@ -0,0 +1,79 @@
+import { Link } from "react-router-dom";
+import { use, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import logo from "../Assets/Img/logo.svg";
+
+function Nav() {
+    const [showNav, setShowNav] = useState(false);
+
+    const handleShowNav = () => {
+        setShowNav(!showNav)
+    }
+
+    return (
+        <header>
+            <Link to={"/"}>
+                <img src={logo} alt="costa logo" className="logo" />
+            </Link>
+            <a id="navToggle" className={`navToggle ${showNav ? "toCross" : ""}`} onClick={handleShowNav}>
+                <span className="navToggleBar navToggleBarTop"></span>
+                <span className="navToggleBar navToggleBarMiddle"></span>
+                <span className="navToggleBar navToggleBarBottom"></span>
+            </a>
+            <nav className={`nav ${showNav ? "navOpen" : ""}`}>
+                <ul className="navList">
+                    <li className="navItem">
+                        <Link to={"/"} className="navLink  textUppercase textBold active">
+                            accueil
+                        </Link>
+                    </li>
+                    <li className="navItem">
+                        <Link to={"/bateau"} className="navLink  textUppercase textBold">
+                            le bateau
+                        </Link>
+                    </li>
+                    <li className="navItem">
+                        <Link className="navLink  textUppercase textBold">
+                            étapes <FontAwesomeIcon icon={faChevronDown} className="icon" />
+                        </Link>
+                        <ul className="subnavList">
+                            <li className="subnavItem">
+                                <Link to={"/Barcelone"} className="subnavLink  textUppercase textBold">
+                                    Barcelone
+                                </Link>
+                            </li>
+                            <li className="subnavItem">
+                                <Link to={"/Cagliari"} className="subnavLink  textUppercase textBold">
+                                    Cagliari
+                                </Link>
+                            </li>
+                            <li className="subnavItem">
+                                <Link to={"/Naples"} className="subnavLink  textUppercase textBold">
+                                    Naples
+                                </Link>
+                            </li>
+                            <li className="subnavItem">
+                                <Link to={"/Civitavecchia"} className="subnavLink  textUppercase textBold">
+                                    Civitavecchia/Rome
+                                </Link>
+                            </li>
+                            <li className="subnavItem">
+                                <Link to={"/Gênes"} className="subnavLink  textUppercase textBold">
+                                    Gênes
+                                </Link>
+                            </li>
+                            <li className="subnavItem">
+                                <Link to={"/Marseille"} className="subnavLink  textUppercase textBold">
+                                    Marseille
+                                </Link>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -1,7 +1,11 @@
+import Nav from "../Components/nav"
+
 function HomePage() {
-    // return (
-        
-    // )
+    return (
+        <div>
+            <Nav />
+        </div>
+    )
 }
 
 export default HomePage
