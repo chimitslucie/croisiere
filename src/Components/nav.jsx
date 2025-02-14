c365873b4b040dd34d8c1033b5a3067f60bcd96c
chimitslucie <chimitslucie@gmail.com>
2025-02-14 08:31:10 +0100

    modification nav

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { use, useState } from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import logo from "../Assets/Img/logo.svg";
