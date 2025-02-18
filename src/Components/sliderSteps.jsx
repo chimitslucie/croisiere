954836fa6be82bf80c623dbefded5a8b26b8d41d
chimitslucie <chimitslucie@gmail.com>
2025-02-18 12:03:40 +0100

    add sliderSteps

---
+++ src/Components/sliderSteps.jsx
@@ -0,0 +1,63 @@
+import Slider from "react-slick";
+import datas from "../Data/data.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+function SliderSteps() {
+
+    const NextArrow = ({ onClick }) => {
+        return (
+            <div className="custom-arrow next" onClick={onClick}>
+                <FontAwesomeIcon icon={faChevronRight} />
+            </div>
+        );
+    };
+
+    const PrevArrow = ({ onClick }) => {
+        return (
+            <div className="custom-arrow prev" onClick={onClick}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+            </div>
+        );
+    };
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 580,
+                settings: {
+                    arrows: false
+                },
+            },
+        ],
+    }
+
+    return (
+        <div className="sliderSteps">
+            <div className="sliderStepsContent">
+                <Slider {...settings}>
+                    {datas.etapes.map((etape, index) => (
+                        <div className="sliderStepsCard" key={index}>
+                            <img src={etape.image} className="image" />
+                            <div className="sliderStepsCardText">
+                                <p className="date textUppercase">{etape.date}</p>
+                                <h2 className="city textUppercase">{etape.ville}</h2>
+                                <p className="text">{etape.text}</p>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+            </div>
+        </div>
+    )
+}
+
+export default SliderSteps
