2f984aeda83da14f81eb1e58a1b1c6660d45d765
chimitslucie <chimitslucie@gmail.com>
2025-03-10 20:31:16 +0100

    modification json files + images

--- src/Pages/Civitavecchia.jsx
+++ src/Pages/Civitavecchia.jsx
@@ -1,7 +1,7 @@
 import Hero from "../Components/hero";
 import Intro from "../Components/Intro";
 import Title from "../Components/Title";
-import datas from "../Data/data.json";
+import civitavecchia from "../Data/civitavecchia.json";
 
 function Civitavecchia() {
     return (
@@ -22,13 +22,48 @@ function Civitavecchia() {
             <section className="visit">
                 <div className="container">
                     <Title title="à voir, à visiter" />
-                    <div className="visitContent"></div>
+                    <div className="visitContent">
+                        {
+                            civitavecchia.voirs.map((voir, index) => (
+                                <div className="card" key={index}>
+                                    <img src={voir.image} />
+                                    <div className="cardContent">
+                                        <h2 className="textUppercase textDarkblue textBold">{voir.name}</h2>
+                                        <p className="text textSecondary textSemibold">{voir.text}</p>
+                                        <p className="trajet textSecondary">Le temps depuis le bateau est {voir.temps}</p>
+                                        <p className="price textSecondary">{voir.price}</p>
+                                        <a href={voir.lien} className="link textDarkblue">{voir.lien}</a>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </section>
             <section className="excursion">
                 <div className="container">
                     <Title title="excursions porposer par costa" />
-                    <div className="excursionContent"></div>
+                    <div className="excursionContent">
+                        {
+                            civitavecchia.excursions.map((excursion, index) => (
+                                <div className="card" key={index}>
+                                    <div className="cardContent">
+                                        <img src={excursion.image} />
+                                        <div className="cardContentText">
+                                            <h2 className="textUppercase textDarkblue textBold">{excursion.name}</h2>
+                                            <p className="time textSecondary">La durée de l'excursion est de {excursion.time}</p>
+                                            <p className="price textSecondary">{excursion.price}</p>
+                                            <p className="text textSecondary textSemibold">{excursion.text}</p>
+                                            <p className="info textSecondary textSemibold">{excursion.info}</p>
+                                            <a href={excursion.lien} className="link textDarkblue">Lien pour réserver: {excursion.lien}</a>
+                                        </div>
+                                        <button className="btn" onClick={handleShow}><FontAwesomeIcon icon={faChevronDown} className="btnIcon" /></button>
+                                    </div>
+                                    <p className={`plus textSecondary textSemibold ${show ? 'show' : ""}`}>{excursion.plus}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </section>
         </div>
